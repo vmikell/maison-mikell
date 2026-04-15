28882f1756daf08fed30eff0f8f9b7823bfd9a60
Henry Vale <henryvale.ai@gmail.com>
2026-04-15 07:07:27 -0400

    Redirect Maison traffic to the working auth host

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const canonicalHost = 'maison-reset.firebaseapp.com'
+
+if (typeof window !== 'undefined' && window.location.hostname !== canonicalHost) {
+  const nextUrl = new URL(window.location.href)
+  nextUrl.protocol = 'https:'
+  nextUrl.host = canonicalHost
+  window.location.replace(nextUrl.toString())
+} else {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
